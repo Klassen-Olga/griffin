1bf56f03dfb83bbd4cd52e9a3c94ba528d9c3ef3
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2020-12-09 12:32:59 +0100

    changed configs database

--- config/database.js
+++ config/database.js
@@ -1,27 +1,27 @@
 require('dotenv').config();
 module.exports = {
 	development: {
-		username: process.env.DB_USER || 'lg2g812x6tpzhjyu',
-		password: process.env.DB_PASSWORD || 'g41yyeiv2nw2d1bv',
-		database: process.env.DB_NAME || 'cs7b61hp4mb2wjxm',
-		host: process.env.DB_HOST || 'lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+		username: process.env.DB_USER || 'root',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'griffinDB',
+		host: process.env.DB_HOST || 'localhost',
 		dialect: 'mysql',
-		port: process.env.DB_PORT || '3306'
+		port: process.env.DB_PORT || '3307'
 	},
 	test: {
-		username: 'lg2g812x6tpzhjyu',
-		password: 'g41yyeiv2nw2d1bv',
-		database: 'cs7b61hp4mb2wjxm',
-		host: 'lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+		username: process.env.DB_USER || 'root',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'griffinDB',
+		host: process.env.DB_HOST || 'localhost',
 		dialect: 'mysql',
-		port: '3306'
+		port: process.env.DB_PORT || '3307'
 	},
 	production: {
-		username: 'lg2g812x6tpzhjyu',
-		password: 'g41yyeiv2nw2d1bv',
-		database: 'cs7b61hp4mb2wjxm',
-		host: 'lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+		username: process.env.DB_USER || 'root',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'griffinDB',
+		host: process.env.DB_HOST || 'localhost',
 		dialect: 'mysql',
-		port: '3306'
+		port: process.env.DB_PORT || '3307'
 	}
 }
