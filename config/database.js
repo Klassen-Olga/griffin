76d0892309f1a394f880d165826aecce0ff4cdef
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2020-12-09 11:50:42 +0100

    added mysql database changes for heroku

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 module.exports = {
 	development: {
-		username: process.env.DB_USER || 'root',
-		password: process.env.DB_PASSWORD || '',
-		database: process.env.DB_NAME || 'griffinDB',
-		host: process.env.DB_HOST || 'localhost',
+		username: process.env.DB_USER || 'lg2g812x6tpzhjyu',
+		password: process.env.DB_PASSWORD || 'g41yyeiv2nw2d1bv',
+		database: process.env.DB_NAME || 'cs7b61hp4mb2wjxm',
+		host: process.env.DB_HOST || 'lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
 		dialect: 'mysql',
-		port: process.env.DB_PORT || '3307'
+		port: process.env.DB_PORT || '3306'
 	},
 	test: {
 		username: 'root',
